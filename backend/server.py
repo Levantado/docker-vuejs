5519ab611ce824a091bb4bb4042dc7372a3d9b15
Anton Oleinik <levantado@me.com>
2019-10-08 16:40:58 +0400

    Travis setup

--- backend/server.py
+++ backend/server.py
@@ -7,18 +7,19 @@ async def init_redis():
     redis = await aioredis.create_redis_pool('redis://redis-server')
     await redis.set('visitors', 0)
     return await redis
+
 routes = web.RouteTableDef()
 
 @routes.get('/get')
 async def visitors(request):
-    global CLI
-    if not CLI:
-        CLI = await init_redis()
     visitors = await CLI.incr('visitors')
     body = {'visitors': visitors}
     return web.json_response(body)
 
-app = web.Application()
-app.add_routes(routes)
+async def init_func(*argv):
+    global CLI
+    CLI = await init_redis()
+    app = web.Application()
+    app.add_routes(routes)
+    return app
 
-web.run_app(app)
